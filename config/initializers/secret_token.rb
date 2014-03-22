dde9a0441b6b5d4158d9c0c0dd1e33c8f4a76a5f
pasuya1981 <pasuya1234@hotmail.com>
2014-03-22 16:10:05 +0800

    Initail Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhotoUploader::Application.config.secret_key_base = 'c188af5803e1351a5c197b85c526152f6e05c21633ab73c960ffcf40cc5eee1c39019aea0c3ac82f685cf78f29dad748bedef0df38477a1a150f8273d3f0e19b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhotoUploader::Application.config.session_store :cookie_store, key: '_photo-uploader_session'
