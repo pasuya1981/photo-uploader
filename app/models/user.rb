21fe3e2b89dbe43bf3669b5209438bd793e3076f
pasuya1981 <pasuya1234@hotmail.com>
2014-03-22 16:50:00 +0800

    add rsepc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  avatar     :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
+
+  mount_uploader :avatar, AvatarUploader
+  validates_presence_of :name, :email
+  validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  avatar     :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "user attributes" do
+  	let(:user) { User.create(name: 'Zack', email: 'example@example.com') }
+
+  	it { user.should respond_to(:name) }
+  	it { user.should respond_to(:email) }
+  	it { user.name.should == 'Zack' }
+  	it { user.email.should == 'example@example.com' }
+  end
 end
