3722151c5d25c6f69c8d87e6681a8227e0e32685
pasuya1981 <pasuya1234@hotmail.com>
2014-03-22 17:18:20 +0800

    upload function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,22 @@ class UsersController < ApplicationController
   end
 
   def new
-  	@use = User.new
+  	@user = User.new
   end
+
+  def create
+  	@user = User.new(secure_params)
+  	if @user.valid?
+  	  @user.save
+  	  flash[:notice] = "User: #{@user.name} has been created"
+  	  redirect_to root_path
+  	else
+  	  render 'new'
+  	end
+  end
+
+  private
+    def secure_params
+      params.require(:user).permit(:name, :email, :avatar)
+    end
 end
